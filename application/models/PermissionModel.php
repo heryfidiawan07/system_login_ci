c0301e5046f7b9846ffbd80554f4805315a4d3b7
heryfidiawan07 <heryfidiawan07@gmail.com>
2020-05-14 13:09:20 +0700

    add parent menu

--- application/models/PermissionModel.php
+++ application/models/PermissionModel.php
@@ -4,12 +4,13 @@ class PermissionModel extends CI_Model {
 
 	public function permission($except = FALSE) {
 		
-		$this->db->select(['users.id', 'users.name as user_name', 'users.username', 'users.email', 'roles.name as role_name', 'roles.description as role_desc', 'menus.id as id_menu', 'menus.name as menu_name', 'menus.controller as menu_controller', 'sub', 'order_key']);
+		$this->db->select(['users.id', 'users.name as user_name', 'users.username', 'users.email', 'roles.name as role_name', 'roles.description as role_desc', 'menus.id as id_menu', 'menus.name as menu_name', 'menus.controller as menu_controller', 'parent_id', 'order_key', 'parents.id as id_parent', 'parents.name as parent_name']);
 		$this->db->from('users');
 		$this->db->join('user_role', 'user_role.user_id = users.id');
 		$this->db->join('roles', 'roles.id = user_role.role_id');
 		$this->db->join('role_menu', 'role_menu.role_id = roles.id');
 		$this->db->join('menus', 'menus.id = role_menu.menu_id');
+		$this->db->join('parents', 'parents.id = menus.parent_id');
 		$this->db->where(['users.email' => $this->session->userdata('email')]);
 		// $this->db->group_by('sub');//authorize menu
 		$this->db->order_by('menus.order_key');
